3831702519b9fe5e1a15ac20fa53acc8db605df0
Harsh Singh <69427539+harshsinn@users.noreply.github.com>
2023-06-26 19:18:14 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,5 +117,7 @@ resumeButton.addEventListener('click', ()=>{
     html2pdf(areaCV, opt)
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
-    setTimeout(removeScaleCV, 2000)
+    // setTimeout(removeScaleCV, 2000)
 })
+    
+
